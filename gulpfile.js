84d46379608ec68f0d23f75680f1c2ddf6c55a05
gregorytomilin <gregory.tomilin@gmail.com>
2020-10-12 00:12:21 +0300

    2й этап сборки

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+const { src, dest, task, series, watch } = require("gulp");
+const svgmin = require('gulp-svgmin');
+const rename = require('gulp-rename');
+const svgSprite = require('gulp-svg-sprite');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
+
+task('icons', () => {
+    return src('./img/icons/svg/*.svg')
+        // minify svg
+        .pipe(svgmin({ js2svg: { pretty: true } }))
+        // replace spaces with hyphens, get rid of the "-icon" postfix
+        .pipe(rename(function (path) {
+            path.basename = path.basename
+                .replace(/\s/g, '-')
+                .replace(/-icon/, '');
+        }))
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    dest: '',
+                    example: true,
+                    sprite: 'vector-icons.min.svg'
+                },
+                inline: false
+            }
+        }))
+        .pipe(dest('./img/icons/'))
+    // .pipe(browserSync.reload({stream: true}));
+});
+
+
+
+task('sass', () => {
+    return src('css/scss/**/*.scss') // берём все sass-файлы
+        .pipe(sass({
+            outputStyle: 'compressed',  // вложенный (по умлчанию)
+        }).on('error', sass.logError))
+        .pipe(sourcemaps.init())
+        .pipe(dest('./css'));      // выгружаем результат
+});
--- index.php
+++ index.php
@@ -5,9 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Bubbles</title>
 </head>
 <body>
+<header>
+
+    <svg class="icon icon-arr_round red_mod">
+        <use xlink:href="i/sprite/sprite.svg#arr_round"></use>
+    </svg>
+</header>
 
 </body>
 </html>
